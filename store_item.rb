8dec64d0f5689b4f3fbff3ce9223435843af4b14
AnnaGulstine <annagulstine@gmail.com>
2016-02-29 21:24:33 -0600

    replaced Mug class parameter with an options hash

--- store_item.rb
+++ store_item.rb
@@ -6,22 +6,28 @@ class Mug
 
   attr_reader :size, :color, :material, :price
 
-  def initialize(input_size, input_color, input_material, input_price)
-    @size = input_size
-    @color = input_color
-    @material = input_material
-    @price = input_price
+  def initialize(input_options)
+    @size = input_options[:size]
+    @color = input_options[:color]
+    @material = input_options[:material]
+    @price = input_options[:price]
   end
 
   def print_info
     puts "#{@size} #{@color} #{@material} #{@price}"
   end
 
+  def annual_price_increase
+    @price = @price * 1.05
+  end
+
 end
 
-mug1 = Mug.new("small", "black", "ceramic", 10)
-mug2 = Mug.new("large", "white", "stoneware", 20)
-mug3 = Mug.new("medium", "brown", "glass", 15)
+# New way of creating instances of the Mug class: (can call options hash)
+mug1 = Mug.new({size: "small", color: "black", material: "ceramic", price: 10})
+mug2 = Mug.new(size: "large", color: "white", material: "stoneware", price: 20)
+mug3 = Mug.new size: "medium", material: "glass", price: 15, color: "brown"
+
 
 mug1.print_info
 mug2.print_info
@@ -31,3 +37,7 @@ puts mug1.size
 puts mug1.color
 puts mug1.material
 puts mug1.price
+
+puts mug1.annual_price_increase
+puts mug2.annual_price_increase
+puts mug3.annual_price_increase
