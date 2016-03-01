dbc6120dccecb3df73208513e5b646b9ad8df1c5
AnnaGulstine <annagulstine@gmail.com>
2016-02-29 20:22:24 -0600

    added some new products

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+mug1 = {size: "small", color: "black", material: "ceramic", price: 10}
+mug2 = {size: "large", color: "white", material: "stoneware", price: 20}
+
+puts "Would you like to buy a #{mug1[:size]} #{mug1[:color]} #{mug1[:material]} mug for #{mug1[:price]} dollars?"
+puts "Or maybe you would prefer a #{mug2[:size]} #{mug2[:color]} #{mug2[:material]} mug for #{mug2[:price]} dollars?"
